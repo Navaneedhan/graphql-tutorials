c6139585312e03112c220cf21a484c0d07b68a71
navaneedhan <navaneedhan.cit@gmail.com>
2019-02-16 17:25:42 +0530

    authentication with graphQL

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,10 +3,15 @@ module Types
     # queries are just represented as fields
     # `all_links` is automatically camelcased to `allLinks`
     field :all_links, [LinkType], null: false
+    field :all_users, [UserType], null: false
 
     # this method is invoked, when `all_link` fields is beeing resolved
     def all_links
       Link.all
+    end
+
+    def all_users
+      User.all
     end
   end
 end
